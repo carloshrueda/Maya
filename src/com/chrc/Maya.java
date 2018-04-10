fa950666b044be550ff990a67d4f69783c1a3449
crueda <carloshrueda@gmail.com>
2018-04-09 20:25:00 -0500

    Creacion de metodo que resuelve el ejercicio de devolver un vector sin elementos enteros repetidos

--- src/com/chrc/Maya.java
+++ src/com/chrc/Maya.java
@@ -1,18 +1,20 @@
 package com.chrc;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Maya {
     public String[] dividirFrase(String fraseOriginal, int longMax) {
         String[] strdiv = null;
         int lfo = fraseOriginal.length();
 
-        if (lfo > longMax){
+        if (lfo > longMax) {
             String[] vpal = fraseOriginal.split(" ");
             strdiv = new String[vpal.length];
             int pvfr = 0;
 
-            strdiv[0]= vpal[0];
+            strdiv[0] = vpal[0];
             for (int i = 1; i < vpal.length; i++)
                 if (strdiv[pvfr].length() + vpal[i].length() + 1 <= longMax)
                     strdiv[pvfr] += " " + vpal[i];
@@ -31,10 +33,29 @@ public class Maya {
         return strdiv;
     }
 
+    public Integer[] noRepetidos(Integer[] vnum) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        if (vnum != null) {
+            try {
+                for (Integer num : vnum)
+                    set.add(num);
+
+            } catch (Exception e) {
+                System.out.printf("Error. " + e.getMessage());
+            }
+
+            return set.toArray(new Integer[set.size()]);
+
+        } else
+            return null;
+    }
+
     public static void main(String[] args) {
         Maya m = new Maya();
-        String frase = "Esta es una frase de prueba con longitud mayor a 10";
-        String[] resultado = m.dividirFrase(frase, 10);
+        int tam = 10;
+        String frase = "Esta es una frase de prueba con longitud mayor a " + tam;
+        String[] resultado = m.dividirFrase(frase, tam);
 
         System.out.println(frase + "\n");
         if (resultado != null) {
@@ -43,7 +64,31 @@ public class Maya {
         } else
             System.out.println("NO hay cadena con ese tamano");
 
-        System.out.println("\n");
+        System.out.println("\n--------- EJERCICIO 2 ---------\n");
 
+        // EJECICIO 2
+
+        Integer[] numeros = {1, 2, 3, 1, 2, 3, 4, 6, 6, 5, 6, 4};
+
+        if (numeros != null) {
+            for (Integer n : numeros) {
+                System.out.print(n.toString() + ", ");
+
+            }
+            System.out.println("");
+
+            Integer[] resultint = m.noRepetidos(numeros);
+
+            if (resultint != null)
+                for (Integer n : resultint)
+                    System.out.print(n.toString() + ", ");
+
+            else
+                System.out.println("NO hay cadena con ese tamano");
+
+        } else
+            System.out.println("NO hay numeros para evaluar");
+
+        System.out.println("\n");
     }
 }
